98163070bb864843e5da6d16ebc1abe85c3500fd
Ricardo Salim Daruix <rsalim@viajanetsp.local>
2019-08-15 19:13:25 -0300

    Inject dependencies, first structures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'teste-ewave';
+  constructor() {
+    library.add(fas, fab);
+  }
 }
